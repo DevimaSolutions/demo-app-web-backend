abb5e98264c9c5f7ca8b7bdb9316577c023bbfe5
NazarDevima <nazar@shaper.us>
2024-03-20 14:18:04 +0200

    fix: fixes after review

--- src/features/profiles/validations/onboarding-first-step.schema.ts
+++ src/features/profiles/validations/onboarding-first-step.schema.ts
@@ -4,7 +4,7 @@ import { Gender } from '@/features/profiles';
 import { OnboardingFirstStepRequest } from '@/features/profiles/dto/request/onboarding-first-step.request';
 export const onboardingFirstStepSchema = Joi.object<OnboardingFirstStepRequest>({
   name: Joi.string().max(255).required(),
-  age: Joi.number().positive().min(0).max(150).required(),
+  age: Joi.number().positive().min(13).max(150).required(),
   gender: Joi.string()
     .valid(...Object.values(Gender))
     .required(),
