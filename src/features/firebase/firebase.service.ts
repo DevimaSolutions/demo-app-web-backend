67ddbd0c15b5767f96e02e55976fb4dc6cbcb36b
BohdanDevima <bohdan@shaper.us>
2024-04-16 15:25:32 +0300

    fix: added get firebase keys from google cloud secret manager

--- src/features/firebase/firebase.service.ts
+++ src/features/firebase/firebase.service.ts
@@ -1,3 +1,4 @@
+import { SecretManagerServiceClient } from '@google-cloud/secret-manager';
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import admin from 'firebase-admin';
@@ -7,20 +8,48 @@ import { UsersService } from '@/features/users';
 
 @Injectable()
 export class FirebaseService {
-  constructor(
-    private readonly usersService: UsersService,
-    private readonly config: ConfigService,
-  ) {}
+  private clientSecretManager: SecretManagerServiceClient;
 
-  public initializeFirebaseAdmin() {
+  constructor(private readonly usersService: UsersService, private readonly config: ConfigService) {
+    this.clientSecretManager = new SecretManagerServiceClient();
+  }
+
+  // Access secret from Google Cloud Secret Manager
+  private async accessGoogleCloudSecret(name: string, version = 'latest') {
+    try {
+      const projectId = this.config.get('google.projectId');
+      if (!projectId) {
+        throw 'Project ID is not defined';
+      }
+      const fullName = `projects/${projectId}/secrets/${name}/versions/${version}`;
+      const [response] = await this.clientSecretManager.accessSecretVersion({ name: fullName });
+      const payload = response.payload?.data?.toString();
+      return payload;
+    } catch (error) {
+      // TODO: add logger
+      throw new BadRequestException('Could not access Google Cloud Secret Manager.');
+    }
+  }
+
+  public async initializeFirebaseAdmin() {
     // TODO: add logger for unsuccessful initialization
     if (!admin.apps.length) {
-      admin.initializeApp({
-        credential: admin.credential.cert(
-          this.config.get<string>('firebase.credentialsFilePath', ''),
-        ),
-        databaseURL: this.config.get<string>('firebase.databaseURL'),
-      });
+      // Use Google Cloud Secret Manager to store credentials on Production
+      const cert =
+        process.env.NODE_ENV === 'production'
+          ? await this.accessGoogleCloudSecret(
+              this.config.get<string>('firebase.credentialsFilePath', ''),
+            )
+          : this.config.get<string>('firebase.credentialsFilePath', '');
+
+      if (cert) {
+        admin.initializeApp({
+          credential: admin.credential.cert(cert),
+          databaseURL: this.config.get<string>('firebase.databaseURL'),
+        });
+      } else {
+        throw new BadRequestException('Could not initialize Firebase Admin.');
+      }
     }
   }
 
